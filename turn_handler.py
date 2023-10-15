335a4a764bc26b157d1e2b8fe2dd3a98e556833d
Leander Stolle <leanderstolle@aol.de>
2023-10-15 17:17:52 +0200

    implemented turnhandler

--- turn_handler.py
+++ turn_handler.py
@@ -1,13 +1,26 @@
 import player
 
 
+
 class TurnHandler:
-    def __init__(self, players):
-        self.players = players
-        self.current_turn = 0
+    def __init__(self, player_order: list):
+        self.player_order = player_order
+        self.current_player_index  = 0
+        self.current_player = self.player_order[self.current_player_index]
+    
+
 
     def next_turn(self):
-        self.current_turn = (self.current_turn + 1) % len(self.players)
+        self.current_player_index = (self.current_player_index + 1) % len(self.player_order)
+        self.current_player = self.player_order[self.current_player_index]
 
-    def get_current_player(self):
-        return self.players[self.current_turn]
+    def is_player_turn(self, player_color):
+        return player_color == self.current_player
+
+    def end_turn(self):
+        self.next_turn()
+        # Add any end-of-turn logic here
+
+    def start_turn(self):
+        pass
+        # Add any start-of-turn logic here
